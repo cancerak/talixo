0fa2ee31e1def9c6f74b5759d1cab9a271402551
cancerak <maciej.spisak@protonmail.com>
2019-05-15 22:48:17 +0200

    final version 0.0.1

--- cars/models.py
+++ cars/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -23,12 +23,12 @@ class CarModel(models.Model):
     )
     category = models.CharField(max_length=8, choices=CATEGORIES)
     engine = models.CharField(max_length=8, choices=ENGINES)
-    name = models.CharField(max_length=100, unique=True)  # same engines/category... for 1 car name
+    name = models.CharField(max_length=100)
     passengers = models.PositiveIntegerField()
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     #
     # class Meta:  # different engines/category... for 1 car name allowed
-    #     unique_together = ('category', 'engine', 'name', 'passengers', 'manufacturer')
+    #     unique_together = ('category', 'engine', 'name', 'passengers')
 
     def __str__(self):
         return str(self.manufacturer) + ' ' + self.name
